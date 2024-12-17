1ef5714ff23648e883898ad5567105b706bbb428
Annu881 <rannaannu@gmail.com>
2024-12-18 00:09:29 +0530

    added home button and sign up/login pages

--- login.js
+++ login.js
@@ -3,7 +3,6 @@ if (!currentUser) {
    alert("No user is logged in. Redirecting to login page...");
    window.location.href = "first.html";
 } else {
-   
    let welcomeMessage = document.getElementById("welcome-message");
    welcomeMessage.textContent = `Hello, ${currentUser.firstName} ${currentUser.lastName}! Welcome to your profile.`;
 }
